6e101b01ff0e539afdb71765d6d5b7d35679520c
lvyiting <90591167+lvyiting@users.noreply.github.com>
2022-12-10 14:53:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oWwV85mJSFEJvUDNGj_sr_IDJPr0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'E-QBtIEhcvB4pdCS3Jmz3ctPeXHbY0gM3qdtUQrMGLo',
+      useTemplateId: 'y0u0HXX8tfqs9yf4CyOufR1snwHJrhKTLR966nrnsWg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
